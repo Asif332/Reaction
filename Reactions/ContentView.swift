3d8b3efbe60e3d119f9389b6601be8fde72fff43
Mohammad Asif <mohammad.asif@publicisgroupe.net>
2021-09-29 22:54:46 +0530

    Initial Commit

---
+++ Reactions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reactions
+//
+//  Created by Mohammad Asif on 29/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reactions/ReactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionsApp.swift
+//  Reactions
+//
+//  Created by Mohammad Asif on 29/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ReactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
